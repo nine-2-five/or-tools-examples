f7f10d1f5c6df0cb4635724ef156f618e3989db0
Ivan Švaljek <ivan.svaljek@gmail.com>
2023-01-24 08:14:44 +0100

    Cleanup

---
+++ or-tools-examples/GoogleDistanceMatrixResponse.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace or_tools_examples
+{
+    public class GoogleDistanceMatrixResponse
+    {
+        public string Status { get; set; }
+
+        [JsonPropertyName("destination_addresses")]
+        public string[] DestinationAddresses { get; set; }
+
+        [JsonPropertyName("origin_addresses")]
+        public string[] OriginAddresses { get; set; }
+
+        [JsonPropertyName("rows")]
+        public Row[] Rows { get; set; }
+
+        public class Data
+        {
+            [JsonPropertyName("value")]
+            public long Value { get; set; }
+
+            [JsonPropertyName("text")]
+            public string Text { get; set; }
+        }
+
+        public class Element
+        {
+            [JsonPropertyName("status")]
+            public string Status { get; set; }
+
+            [JsonPropertyName("duration")]
+            public Data Duration { get; set; }
+
+            [JsonPropertyName("distance")]
+            public Data Distance { get; set; }
+        }
+
+        public class Row
+        {
+            [JsonPropertyName("elements")]
+            public Element[] Elements { get; set; }
+        }
+    }
+}
---
+++ or-tools-examples/Helper.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace or_tools_examples
+{
+    internal class Helper
+    {
+        public static string SerializeArray(long[,] array)
+        {
+            string strArray = "{";
+            for (var i = 0; i < array.GetLength(0); i++)
+            {
+                strArray += "{";
+                for (var j = 0; j < array.GetLength(1); j++)
+                {
+                    strArray += array[i, j] + ",";
+                }
+                strArray += "},\n";
+            }
+            strArray += "}";
+
+            return strArray;
+        }
+
+        public static long[,] GetTimeMatrix(string file)
+        {
+            var googleData = JsonSerializer.Deserialize<GoogleDistanceMatrixResponse>(File.ReadAllText(file));
+
+            var timeMatrix = new long[googleData.Rows.Length, googleData.Rows.Length];
+
+            for (var row = 0; row < googleData.Rows.Length; row++)
+            {
+                for (var col = 0; col < googleData.Rows.Length; col++)
+                {
+                    timeMatrix[row, col] = googleData.Rows[row].Elements[col].Duration.Value / 60;
+                }
+            }
+
+            return timeMatrix;
+        }
+    }
+}
